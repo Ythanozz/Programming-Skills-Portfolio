1d1d0bed6abdc2b62d077fef27fbc0b9d1528aed
Ythanozz <ythan2006@gmail.com>
2024-10-21 17:39:09 +0400

    Add files via upload

---
+++ Exercise4_AdvanceReq.py
@@ -0,0 +1,38 @@
+x1 = (input("What is the capital of France? "))
+x2 = (input("What is the capital of Albania? "))
+x3 = (input("What is the capital of Croatia? "))
+x4 = (input("What is the capital of Latvia? "))
+x5 = (input("What is the capital of Poland? "))
+x6 = (input("What is the capital of Russia? "))
+x7 = (input("What is the capital of Sweden? "))
+x8 = (input("What is the capital of Ukraine? "))
+x9 = (input("What is the capital of Norway? "))
+x10 = (input("What is the capital of Ireland? "))
+
+x1 = x1.lower()
+x2 = x2.lower()
+x3 = x3.lower()
+x4 = x4.lower()
+x5 = x5.lower()
+x6 = x6.lower()
+x7 = x7.lower()
+x8 = x8.lower()
+x9 = x9.lower()
+x10 = x10.lower()
+
+if x1 == "paris":
+    if x2 == "tirana":
+        if x3 == "zagreb":    
+            if x4 == "riga":
+                if x5 == "warsaw":
+                    if x6 == "moscow":
+                        if x7 == "stockholm":
+                            if x8 == "kyiv":
+                                if x9 == "oslo":
+                                    if x10 == "dublin":
+       
+                                        print("Nice one")
+
+else:
+        print("One of your answers is wrong.Try again.")
+        
